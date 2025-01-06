34bed2a33b5715f7b7ccfc7bf25f499d612b964d
Jesus Parra <parrajesusalejandro@gmail.com>
2025-01-06 05:13:41 -0400

    add routing to home and auth components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {component:HomeComponent, path:""},
+  {component:SellerAuthComponent, path:"seller-auth"}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
