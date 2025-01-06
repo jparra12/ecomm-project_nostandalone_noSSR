646c08cefbf16c51bf88fb3cd87d8f422dbcf438
Jesus Parra <parrajesusalejandro@gmail.com>
2025-01-05 22:32:06 -0400

    Redo what was done but nostandalone and no SSR

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-auth',
+  standalone: false,
+  
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+
+}
